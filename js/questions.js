12948734cd9e4f77ba42e91f5c8c52394971e694
vic3008 <oluwasegunkorede02@gmail.com>
2025-02-09 05:01:16 -0800

    added: styling and improve functionality

---
+++ js/questions.js
@@ -0,0 +1,557 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+      "The Passion of the God",
+      "The Passion Self",
+      "The Passion of the Christ",
+      "The Passion of the Lord"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who was the first televised President?",
+    answer: "Franklin D. Roosevelt",
+    options: [
+        "Frank Roosevelt",
+        "Franklin D. Roosevelt",
+        "Franklin K. Roosevelt",
+        "GeFranklin Roosevelt"
+    ]
+},
+{
+    numb: 8,
+    question: "Pocahontas was baptized and given what English name?",
+    answer: "Rebecca",
+    options: [
+        "Mary",
+        "Rebecca",
+        "Esther",
+        "Ruth"
+    ]
+},
+{
+    numb: 9,
+    question: "Originally, Amazon only sold what kind of product??",
+    answer: "Books",
+    options: [
+        "America book",
+        "Law Books",
+        "Books",
+        "Germany books"
+    ]
+},
+{
+    numb: 10,
+    question: " In 2009, what became the first Morse code character to be added since WWII?",
+    answer: "The @ symbol",
+    options: [
+        "App @ symbol",
+        "The @ symbol",
+        " @ symbol",
+        "Websymbol",
+    ]
+},
+{
+    numb: 11,
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo da Vinci",
+    options: [
+        "Leonardo Poga",
+        "London Vincit",
+        "Leonardo da Vinci",
+        "German Leonardo",
+    ]
+},
+{
+    numb: 12,
+    question: "Who was the first woman to win a Nobel Prize?",
+    answer: "Marie Curie (1903)",
+    options: [
+        "Marie Curie (1907)",
+        "Marie Curie (1963)",
+        "Mary Curie (1903)",
+        "Esther Curie (1938)"
+    ]
+},
+{
+    numb: 13,
+    question: "Which kind of bulbs were once exchanged as a form of currency?",
+    answer: "Tulips",
+    options: [
+        "ATulipses",
+        "Tullips",
+        "Tulips",
+        "G Tulips"
+    ]
+},
+{
+    numb: 14,
+    question: "What was the original purpose of the tiny pocket in jeans?",
+    answer: "To store pocket watches.",
+    options: [
+        "store pocket",
+        "To store pocket watches.",
+        "store pocket watches.",
+        "pocket watches."
+    ]
+},
+{
+    numb: 15,
+    question: "What does SPF in sunscreen stand for?",
+    answer: "Sun Protection Factor",
+    options: [
+        "Sun Protection",
+        "Sun Protection Factor",
+        "Sun Protect",
+        "Protection Factor"
+    ]
+},
+{
+    numb: 16,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "Japan",
+    options: [
+        "America",
+        "Japan",
+        "Nigeria",
+        "Germany",
+    ]
+},
+{
+    numb: 17,
+    question: "Chrome, Safari, Firefox and Explorer are different types of what?",
+    answer: "Web browsers",
+    options: [
+        "Web browsers",
+        "App browsers",
+        "Website browsers",
+        "GWeb browsers"
+    ]
+},
+{
+    numb: 18,
+    question: "In what year was the Internet opened to the public?",
+    answer: "1993",
+    options: [
+        "1666",
+        "1884",
+        "1993",
+        "1450"
+    ]
+},
+{
+    numb: 19,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "Japan",
+    options: [
+        "America",
+        "London",
+        "NJapan",
+        "Germany"
+    ]
+},
+{
+
+    numb: 20,
+    question: "Who is often credited with creating the world's first car?",
+    answer: "JKarl Benz",
+    options: [
+        "AmJKarl Benz",
+        "JKarl Benz",
+        "NKarl Benz",
+        "GKarl Benz"
+    ]
+},
+{
+    numb: 21,
+    question: "Who was the first woman pilot to fly solo across the Atlantic?",
+    answer: "Amelia Earhart",
+    options: [
+        "America",
+        "London",
+        "Amelia Earhart",
+        "Germany"
+    ]
+},
+{
+  
+    numb: 22,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "Japan",
+    options: [
+        "America",
+        "London",
+        "Japan",
+        "Germany"
+    ]
+},
+{
+  
+    numb: 23,
+    question: "When did Facebook first launch?",
+    answer: "2004",
+    options: [
+        "2004",
+        "2010",
+        "2011",
+        "2000"
+    ]
+},
+{
+    numb: 24,
+    question: " 401(k) is a type of tax-advantaged defined-contribution account designed to help you save for what withdrawal from working life?",
+    answer: "Retirement",
+    options: [
+        "America",
+        "London",
+        "Payment",
+        "Retirement"
+    ]
+},
+{
+    numb: 25,
+    question: "What is the last name of the business tycoon behind the No. 5 perfume?",
+    answer: "Chanel",
+    options: [
+        "Amicano",
+        "Chanel",
+        "Cantee",
+        "Buzzy"
+    ]
+},
+{
+    numb: 26,
+    question: "Some of our favorite collective nouns for animals include a congregation of alligators, a business of ferrets, an ostentation of peacocks and a gaggle of what other fowl?",
+    answer: "Geese",
+    options: [
+        "America",
+        "London",
+        "Nigeria",
+        "Germany",
+    ]
+},
+{
+    numb: 27,
+    question: "The digital yuan is giving Alipay a run for its money as the largest mobile payment network based in what country?",
+    answer: "China",
+    options: [
+        "America",
+        "London",
+        "China",
+        "Germany"
+    ]
+},
+{
+    numb: 28,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "America",
+    options: [
+        "America",
+        "London",
+        "Nigeria",
+        "Germany"
+    ]
+},
+{
+    numb: 29,
+    question: "What is the human body's heaviest organ?",
+    answer: "The skin",
+    options: [
+        "Nose",
+        "Tounge",
+        "The head",
+        "The skin"
+    ]
+},
+{
+    numb: 30,
+    question: " Where is the Mona Lisa painting on display?",
+    answer: "The Louvre Museum",
+    options: [
+        "America Museum",
+        "London Museum",
+        "The Louvre Museum",
+        "Germany Museum"
+    ]
+},
+{
+    numb: 31,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "Nigeria",
+    options: [
+        "America",
+        "London",
+        "Nigeria",
+        "Germany"
+    ]
+},
+{
+    numb: 32,
+    question: "What is the longest day of the year?",
+    answer: "The summer solstice",
+    options: [
+        "America",
+        "London",
+        "The summer solstice",
+        "Germany winter"
+    ]
+},
+{
+    numb: 33,
+    question: "What is the sixth sign of the zodiac?",
+    answer: "Virgo",
+    options: [
+        "Virgo",
+        "London",
+        "Nigeria",
+        "Germany"
+    ]
+},
+{
+    numb: 34,
+    question: "When is National Ice Cream month?",
+    answer: "July",
+    options: [
+        "October",
+        "Feburay",
+        "May",
+        "March",
+    ]
+},
+{
+    numb: 35,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "Japan",
+    options: [
+        "America",
+        "London",
+        "Japan",
+        "Germany"
+    ]
+},
+{
+    numb: 36,
+    question: "What is the best-selling book of all time?",
+    answer: "The Bible",
+    options: [
+        "Qurana",
+        "Budahism",
+        "Love Book",
+        "The Bible"
+    ]
+},
+{
+    numb: 37,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "Japan",
+    options: [
+      "Garnet",
+      "Japan",
+      "Sacramento (California)",
+      "France"
+    ]
+},
+{
+    numb: 38,
+    question: "What is January's birthstone?",
+    answer: "Garnet",
+    options: [
+      "Garnet",
+      "The Passion of the Christ",
+      "Sacramento (California)",
+      "France"
+    ]
+},
+{
+    numb: 39,
+    question: "In which city did the famous film Lady Bird take place?",
+    answer: "Sacramento (California)",
+    options: [
+        "America",
+        "The Passion of the Christ",
+        "Sacramento (California)",
+        "France"
+    ]
+},
+{
+    numb: 40,
+    question: "In what country did the first Starbucks open outside of North America?",
+    answer: "Japan",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 41,
+    question: "Who provided the voice of Groot in the Guardians of the Galaxy movies?",
+    answer: "Vin Diesel",
+    options: [
+        "America",
+        "London",
+        "Nigeria",
+        "Germany",
+    ]
+},
+{
+    numb: 42,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+       "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 43,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 44,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 45,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 46,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 47,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "America",
+        "London",
+        "Nigeria",
+        "Germany",
+    ]
+},
+{
+    numb: 48,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 49,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+{
+    numb: 50,
+    question: "Which is the highest-grossing R-rated film in North America?",
+    answer: "The Passion of the Christ",
+    options: [
+        "Peace",
+        "The Passion of the Christ",
+        "Christ movement",
+        "God"
+    ]
+},
+  
+];
